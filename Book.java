732024c3d2374ef58cf5bec1a898be9b87740789
Jserna20 <31867660+Jserna20@users.noreply.github.com>
2017-09-25 12:43:28 -0400

    Ex 2.83
    added getAuthor and getTitle accessor methods

--- Book.java
+++ Book.java
@@ -23,4 +23,15 @@ class Book
     }
 
     // Add the methods here ...
+    
+    //Return author name
+    public String getAuthor()
+    {
+        return author;
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    }
 }
