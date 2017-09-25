05959ddfdf4f34bde3b6be6a74349372150d7402
Jserna20 <31867660+Jserna20@users.noreply.github.com>
2017-09-25 13:05:27 -0400

    2.85
    added field, pages, and getPages accesor to the Book class with comments.

--- Book.java
+++ Book.java
@@ -11,15 +11,19 @@ class Book
     // The fields.
     private String author;
     private String title;
+    // Extra field from 2.85
+    private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    // Edit in constructor to include page number
+    public Book(String bookAuthor, String bookTitle, int pageNumber)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = pageNumber;
     }
 
     // Add the methods here ...
@@ -34,6 +38,13 @@ class Book
     public String getTitle()
     {
         return title;
+    }
+    
+    // Accessor from 2.85
+    // Returns page number
+    public int getPages()
+    {
+        return pages;
     }
     
     // Methods added from 2.84
